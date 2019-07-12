e3b456f84494b15a9ab9afd3b5c99780d9250c7f
sun <sun@example.com>
2019-07-12 08:58:17 +0800

    1

--- qinghuajiqiren/pages/basics/atlas/atlas.js
+++ qinghuajiqiren/pages/basics/atlas/atlas.js
@@ -54,7 +54,7 @@ Page({
     // ec: {
     //   onInit: initChart
     // },
-
+    CustomBar: app.globalData.CustomBar,
     ec: {
       // 将 lazyLoad 设为 true 后，需要手动初始化图表
       lazyLoad: true
@@ -63,6 +63,78 @@ Page({
     isDisposed: false,
     datatmep_m:[],
     linktemp_m: [],
+    all_data:[]
+  },
+  showModal(e) {
+   // console.log(111)
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  show_canvars(e) {
+    console.log(e.currentTarget.dataset.target)
+    var inde = e.currentTarget.dataset.target
+    var tempname = this.data.all_data[inde].name;
+    var datatmeplist = this.data.all_data[inde].rel;
+    var datatmep = [];
+    var linktemp = [];
+
+    datatmep.push({ name: tempname });
+    datatmeplist.forEach(function (value, i) {
+      if (i < 5) {
+        let t = {}
+        t.name = value.name;
+        datatmep.push(t);
+
+        let link = {}
+        link.source = tempname;
+        link.target = value.name;
+        link.label = {
+          normal: {
+            show: true,
+            formatter: value.relationType
+          }
+        };
+        linktemp.push(link);
+      }
+      //console.log( value);
+    })
+
+         console.log(datatmep);
+         console.log(linktemp);
+
+    this.setData({
+      datatmep_m: datatmep,
+      linktemp_m: linktemp,
+    })
+
+      this.ecComponent = this.selectComponent('#mychart-dom-pie');
+      this.ecComponent.init((canvas, width, height) => {
+        // 获取组件的 canvas、width、height 后的回调函数
+        // 在这里初始化图表
+        const chart = echarts.init(canvas, null, {
+          width: width,
+          height: height
+        });
+        setOption(chart, this.data.datatmep_m, this.data.linktemp_m);
+
+        // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+        this.chart = chart;
+
+        this.setData({
+          isLoaded: true,
+          isDisposed: false,
+          modalName: null
+        });
+
+        // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+        return chart;
+      });
   },
   onLoad: function () {
     
@@ -105,6 +177,7 @@ Page({
         that.setData({
           datatmep_m: datatmep,
           linktemp_m: linktemp,
+          all_data: res.data
         })
       }
 
