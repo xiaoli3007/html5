f348b51ceecf26e5185bedda77b1ce94e8e80c22
sun <sun@example.com>
2019-09-09 17:33:13 +0800

    1

--- youyoushizi_xcx/pages/basics/jiaocai_lession/jiaocai_lession.js
+++ youyoushizi_xcx/pages/basics/jiaocai_lession/jiaocai_lession.js
@@ -7,9 +7,11 @@ Page({
     MainCur: 0,
     TabCur: 0,
     scrollLeft: 0,
+    VerticalNavTop: 0,
     // tabNav: ['一年级', '二年级', '三年级'],
     jiaocaiList: [],
-    loadModal: true
+    loadModal: true,
+    load: true
   },
   onLoad(options) {
 
@@ -27,10 +29,17 @@ Page({
       }, 
       success(res) {
 
-        console.log(res.data)
+       
+        var temp_array = []
+        res.data.items.forEach(function (value, i) {
+          let temp = value
+          temp.id = i
+          temp_array.push(temp)
+        })
+        // console.log(temp_array)
         that.setData({
 
-          jiaocaiList: res.data.items
+          jiaocaiList: temp_array
         })
 
       }, 
@@ -54,7 +63,42 @@ Page({
   tabSelect(e) {
     this.setData({
       TabCur: e.currentTarget.dataset.id,
-      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+      MainCur: e.currentTarget.dataset.id,
+      VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
     })
+  },
+  
+  VerticalMain(e) {
+    let that = this;
+    let list = this.data.jiaocaiList;
+    let tabHeight = 0;
+    if (this.data.load) {
+      for (let i = 0; i < list.length; i++) {
+        let view = wx.createSelectorQuery().select("#main-" + list[i].id);
+        view.fields({
+          size: true
+        }, data => {
+          list[i].top = tabHeight;
+          tabHeight = tabHeight + data.height;
+          list[i].bottom = tabHeight;
+        }).exec();
+      }
+      that.setData({
+        load: false,
+        jiaocaiList: list
+      })
+      console.log(list)
+    }
+    let scrollTop = e.detail.scrollTop + 20;
+    console.log(scrollTop)
+    for (let i = 0; i < list.length; i++) {
+      if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+        that.setData({
+          VerticalNavTop: (list[i].id - 1) * 50,
+          TabCur: list[i].id
+        })
+        return false
+      }
+    }
   }
 })
