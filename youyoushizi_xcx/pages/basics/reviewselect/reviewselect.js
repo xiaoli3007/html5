2cbd96055b4a93fa8c84e9c554ba60f21967d506
sun <sun@example.com>
2019-10-22 09:02:19 +0800

    1

--- youyoushizi_xcx/pages/basics/reviewselect/reviewselect.js
+++ youyoushizi_xcx/pages/basics/reviewselect/reviewselect.js
@@ -10,11 +10,46 @@ Page({
     autoplay: false,
     interval: 5000,
     duration: 1000,
-    numsvalue:25
+    numsvalue:25,
+    review_num: 0
   },
 
   onLoad: function () {
     app.setUserInfo('about');
+
+    var that = this;
+
+    // that.setData({
+    //   isLoad: true,
+    // })
+
+    wx.request({
+      url: app.globalData.url + '?act=review_info', //
+      data: {
+        userid: app.globalData.userid ? app.globalData.userid : 0
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'X-Token': app.globalData.xtoken
+      },
+      success(res) {
+
+        console.log(res.data)
+
+        that.setData({
+          review_num: parseInt(res.data.review_num),
+        })
+ 
+      },
+      complete(res) {
+
+        // that.setData({
+        //   isLoad: false,
+        // })
+
+      }
+    })
+
   },
   
   slider4change: function (e) {
@@ -23,6 +58,27 @@ Page({
     this.setData({
       numsvalue: e.detail.value
     })
+  }, 
+  goreview(e) {
+
+    // let taskid = e.currentTarget.dataset.taskid
+    // var that = this
+    // console.log(typeof  this.data.review_num)
+    if(this.data.review_num>0){
+      wx.navigateTo({
+        url: '/pages/basics/review/review?nums=' + this.data.numsvalue,
+      })
+    }else{
+
+      wx.showToast({
+        title: '没有要复习的数据！',
+        icon: 'none',
+        duration: 1500,
+      })
+
+    }
+   
+
   },
 
 })
