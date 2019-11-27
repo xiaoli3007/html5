c1c70b1231e41d582cf506b32a0fced3bdb36f22
sun <sun@example.com>
2019-11-27 16:25:15 +0800

    1

--- youyoushizi_xcx/pages/about/account/account.js
+++ youyoushizi_xcx/pages/about/account/account.js
@@ -341,62 +341,62 @@ Page({
     }
 
   },
-  formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-    const params = e.detail.value
-    //校验表单
-    if (!this.WxValidate.checkForm(params)) {
-      const error = this.WxValidate.errorList[0]
-      this.showFormModal(error)
-      return false
-    }
-    var that = this;
-    wx.request({
-      url: app.globalData.url2 + '?act=wx_edit_password',
-      method: 'POST',
-      header: {
-         'content-type': 'application/x-www-form-urlencoded', // 默认值
-        'X-Token': app.globalData.xtoken
-      },
-      data: {
-        userid: app.globalData.userid,
-        username: params.username,
-        newpasswd: params.newpasswd,
-        confimpasswd: params.confimpasswd,
-      },
-      success: function (res) {
-        console.log(res);
+  // formSubmit: function (e) {
+  //   console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  //   const params = e.detail.value
+  //   //校验表单
+  //   if (!this.WxValidate.checkForm(params)) {
+  //     const error = this.WxValidate.errorList[0]
+  //     this.showFormModal(error)
+  //     return false
+  //   }
+  //   var that = this;
+  //   wx.request({
+  //     url: app.globalData.url2 + '?act=wx_edit_password',
+  //     method: 'POST',
+  //     header: {
+  //        'content-type': 'application/x-www-form-urlencoded', // 默认值
+  //       'X-Token': app.globalData.xtoken
+  //     },
+  //     data: {
+  //       userid: app.globalData.userid,
+  //       username: params.username,
+  //       newpasswd: params.newpasswd,
+  //       confimpasswd: params.confimpasswd,
+  //     },
+  //     success: function (res) {
+  //       console.log(res);
 
-        if (res.data.code === 20001) {
+  //       if (res.data.code === 20001) {
 
-          that.showFormModal({
-            msg: '提交失败'
-          }) 
+  //         that.showFormModal({
+  //           msg: '提交失败'
+  //         }) 
 
-        } else {
-          that.showFormModal({
-            msg: '提交成功'
-          }) 
+  //       } else {
+  //         that.showFormModal({
+  //           msg: '提交成功'
+  //         }) 
 
-        }
+  //       }
 
-      }, complete(res) {
+  //     }, complete(res) {
         
-        that.setData({
-          modalName: null
-        })
-        //console.log(res.statusCode)
-        if (res.statusCode == 500) {
+  //       that.setData({
+  //         modalName: null
+  //       })
+  //       //console.log(res.statusCode)
+  //       if (res.statusCode == 500) {
 
-        } else {
+  //       } else {
 
-        }
+  //       }
 
-      }
-    });
+  //     }
+  //   });
    
     
-  },
+  // },
   //报错 
   showFormModal(error) {
     wx.showModal({
