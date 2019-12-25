d3f5af8e6936c6afe03bf3f1c8868f6c55b05e79
sun <sun@example.com>
2019-12-25 11:30:39 +0800

    1

--- youyoushizi_xcx/pages/basics/collection/collection.js
+++ youyoushizi_xcx/pages/basics/collection/collection.js
@@ -64,17 +64,25 @@ Page({
         success: function (res) {
           // console.log(res);
 
-          console.log(res.data.items.length)
-          if (res.data.items.length < that.data.pagesize || res.data.items.length === 0) {
+          // console.log(res.data.items.length)
+          if (res.data.items){
+            if (res.data.items.length < that.data.pagesize || res.data.items.length === 0) {
+              that.setData({
+                isLoad: true,
+                isend: true,
+              })
+            }
+
+            that.setData({
+              collection_list: res.data.items,
+            })
+          }else{
             that.setData({
               isLoad: true,
               isend: true,
             })
           }
           
-          that.setData({
-            collection_list: res.data.items,
-            })
          }, complete(res) {
            that.setData({
 
