82ef7b466cab408a5e323e506120d04e9610b69d
sun <sun@example.com>
2020-08-05 16:03:47 +0800

    1

--- youyoushizi/config/dev.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5c6bafca65169d1422574f8a/vue-admin"'
-}
--- youyoushizi/config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/5c6bafca65169d1422574f8a/vue-admin"'
-}
