0a83a72a469514cc8b8c9e0a93c16acf84d5717b
Your Name <you@example.com>
2021-02-05 18:45:09 +0800

    upload

--- resadmin/src/components/myupload.vue
+++ resadmin/src/components/myupload.vue
@@ -1,159 +1,157 @@
 <template>
-<div id="" style=" margin:0 auto; width: 100%; text-align: center;">
-	
-	  <el-upload
-	    class="upload-demo" name="fileToUpload"
-	    drag
-	    :action="upload_url" :on-change="handleChange" :on-progress="handleprogress" :on-success="handleAvatarSuccess"
-	 :before-upload="beforeAvatarUpload" :data="updateuserdata"
-	    multiple>
-	    <i class="el-icon-upload"></i>
-	    <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-	    <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-	  </el-upload>
-	  
-	  
-  </div>
+	<div id="" style=" margin:0 auto; width: 100%; text-align: center;">
+
+		<el-upload class="upload-demo" name="fileToUpload" drag :action="upload_url" :on-change="handleChange" :on-progress="handleprogress"
+		 :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload" :data="updateuserdata" multiple>
+			<i class="el-icon-upload"></i>
+			<div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+			<div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+		</el-upload>
+
+
+	</div>
 </template>
 
 <script>
 	// import E from "wangeditor";
 	import {
-		 md5
+		md5
 	} from '@/utils/md5'
 	export default {
-	  name: "myupload",
-	  data() {
-		return {
-			 siteid:1,
-			 username:'master',
-			 miyao:'38408956',
-			updateuserdata:{},
-			 //文件上传服务器地址，如果在别的Web服务器里，请用绝对地址，例如：
-		 upload_url :  "http://8.131.245.14:8023/upload/video",
-		
-		   uploadsrv_url :  "http://8.131.245.14:8021/upload/document",
-		   resume_info_url : 'http://8.131.245.14:8021/resume/',
-		   upload_file_url : 'http://8.131.245.14:8021/upload/video',
-		  
-	    };
-	  },
-	 props: {
-	 	v_model_field: {
-	 		type: String,
-	 		default: ''
-	 	},
-		v_model_name: {
-			type: String,
-			default: ''
+		name: "myupload",
+		data() {
+			return {
+				siteid: 1,
+				username: 'master',
+				miyao: '38408956',
+				updateuserdata: {},
+				//文件上传服务器地址，如果在别的Web服务器里，请用绝对地址，例如：
+				upload_url: "http://8.131.245.14:8023/upload/video",
+
+				uploadsrv_url: "http://8.131.245.14:8021/upload/document",
+				resume_info_url: 'http://8.131.245.14:8021/resume/',
+				upload_file_url: 'http://8.131.245.14:8021/upload/video',
+
+			};
 		},
-		v_model_val: {
-			type: String,
-			default: ''
+		props: {
+			v_model_field: {
+				type: String,
+				default: ''
+			},
+			v_model_name: {
+				type: String,
+				default: ''
+			},
+			v_model_val: {
+				type: String,
+				default: ''
+			},
 		},
-	   },
-	  mounted() {
-		// console.log(this.v_model_val)
-		// this.editorContent = this.v_model_val;
-	   
-		//   this.$emit("passtoparent", v)
-		
-	   // console.log(md5('11111'))
-	   this.updateuserdata={"siteid":this.siteid,"username":this.username,"addtime":this.siteid,"magic":this.miyao}
-	   
-	   // fileid
-	},
-	methods:{
-	  handleChange(file, fileList) {
-	             // this.fileList = fileList.slice(-3);
-				 console.log('handleChange----')
-				 console.log(file)
-				 let fid = this.getFileId (file.raw) ;
-				 
-				 this.updateuserdata.fileid = fid
-				  console.log(fid)
-				  console.log('handleChange----')
-	  }, 
-				
-	handleprogress(event, file, fileList) {
-        // this.fileList = fileList.slice(-3);
-		 console.log('handleprogress----')
-		console.log(file)
-		 console.log('handleprogress---')
-      },
-		 handleAvatarSuccess(res, file) {
-		 	console.log(res)
-		 	console.log(file)
-		 	// this.imageUrl = URL.createObjectURL(file.raw);
-		 	// this.programForm[res.field] = res.fileurl
-		  
-		 },
-		 beforeAvatarUpload(file) {
-		 	// const isJPG = file.type === 'image/jpeg';
-		 	const isLt2M = file.size / 1024 / 1024 < 20000;
-		 
-		 	// if (!isJPG) {
-		 	//   this.$message.error('上传头像图片只能是 JPG 格式!');
-		 	// }
-		 	if (!isLt2M) {
-		 		this.$message.error('上传大小不能超过 20g!');
-		 	}
-		 	return isLt2M;
-		 },
-		 //简单的Cookie帮助函数
-		  setCookie(cname,cvalue,exdays)
-		 {
-		   var d = new Date();
-		   d.setTime(d.getTime()+(exdays*24*60*60*1000));
-		   var expires = "expires="+d.toGMTString();
-		   document.cookie = cname + "=" + cvalue + "; " + expires;
-		 },
-		 
-		  getCookie(cname)
-		 {
-		   var name = cname + "=";
-		   var ca = document.cookie.split(';');
-		   for(var i=0; i<ca.length; i++) 
-		   {
-		     var c = ca[i].trim();
-		     if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-		   }
-		   return "";
-		 },
-		 getFileId (file) 
-		 {
-		     //给浏览器授予一个唯一的ID用于区分不同的浏览器实例（不同机器或者相同机器不同厂家的浏览器）
-		 	 var clientid = this.getCookie("HUAYIUPLOAD");
-		 	// var clientid = {};
-		 	if (clientid == "") {
-		 		//用一个随机值来做浏览器的ID，将作为文件HASH值的一部分
-		 		var rand = parseInt(Math.random() * 1000);
-		 		var t = (new Date()).getTime();
-		 		clientid =rand+'T'+t;
-		 		
-		 		 this.setCookie("HUAYIUPLOAD",clientid,365);
-		 	}
-		 	
-		 	var info = clientid;
-		 	if (file.lastModified)
-		 		info += file.lastModified;
-		 	if (file.name)
-		 		info += file.name;
-		 	if (file.size)
-		 		info += file.size;
-		 	//https://cdn.bootcss.com/blueimp-md5/2.10.0/js/md5.min.js
-		 	var fileid = md5(info);
-		 	return fileid;
-		 },
+		mounted() {
+			// console.log(this.v_model_val)
+			// this.editorContent = this.v_model_val;
+
+			//   this.$emit("passtoparent", v)
+
+			// console.log(md5('11111'))
+
+			let temptime = Date.parse(new Date()) / 1000
+
+			let magic = md5(this.siteid + '_' + this.username + '_' + temptime + '_' + this.miyao).toLowerCase();
+
+			this.updateuserdata = {
+				"siteid": this.siteid,
+				"username": this.username,
+				"addtime": temptime,
+				"magic": magic
+			}
+
+			// fileid
+		},
+		methods: {
+			handleChange(file, fileList) {
+				// this.fileList = fileList.slice(-3);
+				console.log('handleChange----')
+				console.log(file)
+				let fid = this.getFileId(file.raw);
+
+				this.updateuserdata.fileid = fid
+				console.log(fid)
+				console.log('handleChange----')
+			},
+
+			handleprogress(event, file, fileList) {
+				// this.fileList = fileList.slice(-3);
+				console.log('handleprogress----')
+				console.log(file)
+				console.log('handleprogress---')
+			},
+			handleAvatarSuccess(res, file) {
+				console.log(res)
+				console.log(file)
+				// this.imageUrl = URL.createObjectURL(file.raw);
+				// this.programForm[res.field] = res.fileurl
+
+			},
+			beforeAvatarUpload(file) {
+				// const isJPG = file.type === 'image/jpeg';
+				const isLt2M = file.size / 1024 / 1024 < 20000;
+
+				// if (!isJPG) {
+				//   this.$message.error('上传头像图片只能是 JPG 格式!');
+				// }
+				if (!isLt2M) {
+					this.$message.error('上传大小不能超过 20g!');
+				}
+				return isLt2M;
+			},
+			//简单的Cookie帮助函数
+			setCookie(cname, cvalue, exdays) {
+				var d = new Date();
+				d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+				var expires = "expires=" + d.toGMTString();
+				document.cookie = cname + "=" + cvalue + "; " + expires;
+			},
+
+			getCookie(cname) {
+				var name = cname + "=";
+				var ca = document.cookie.split(';');
+				for (var i = 0; i < ca.length; i++) {
+					var c = ca[i].trim();
+					if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+				}
+				return "";
+			},
+			getFileId(file) {
+				//给浏览器授予一个唯一的ID用于区分不同的浏览器实例（不同机器或者相同机器不同厂家的浏览器）
+				var clientid = this.getCookie("HUAYIUPLOAD");
+				// var clientid = {};
+				if (clientid == "") {
+					//用一个随机值来做浏览器的ID，将作为文件HASH值的一部分
+					var rand = parseInt(Math.random() * 1000);
+					var t = (new Date()).getTime();
+					clientid = rand + 'T' + t;
+
+					this.setCookie("HUAYIUPLOAD", clientid, 365);
+				}
+
+				var info = clientid;
+				if (file.lastModified)
+					info += file.lastModified;
+				if (file.name)
+					info += file.name;
+				if (file.size)
+					info += file.size;
+				//https://cdn.bootcss.com/blueimp-md5/2.10.0/js/md5.min.js
+				var fileid = md5(info).toLowerCase();
+				return fileid;
+			},
+		}
 	}
-}
 </script>
 <style lang="scss" scoped>
-
-  
-  
-  .row {
-  	margin-bottom:10px;
-  }
-
+	.row {
+		margin-bottom: 10px;
+	}
 </style>
