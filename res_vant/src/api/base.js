154b6135cbe1cc5fe07c56a9053f13acbe103f54
Your Name <you@example.com>
2021-02-26 12:04:16 +0800

    1

--- res_vant/src/api/base.js
+++ res_vant/src/api/base.js
@@ -1,60 +1,61 @@
-import {  get ,post} from '@/utils/ajax.js'
+import {
+	get,
+	post
+} from '@/utils/ajax.js'
 
 export function index_home(params) {
-  return get( 
-     'vue_app_phone_api.php?act=home',
-      params
-   )
-} 
+	return get(
+		'vue_app_phone_api.php?act=home',
+		params
+	)
+}
 
 export function getshow(params) {
-  return get(
-    'vue_app_content_api.php?act=detail',
-    params
-  )
+	return get(
+		'vue_app_content_api.php?act=detail',
+		params
+	)
 }
 
 export function media_detail(params) {
 	return post(
-   'vue_app_content_api.php?act=media_detail',
-    params,
-	true
- )
+		'vue_app_media_api.php?act=media_detail',
+		params,
+		true
+	)
 }
 
 export function media_frequency(params) {
 	return post(
-   'vue_app_content_api.php?act=media_frequency',
-    params,
-	true
- )
+		'vue_app_media_api.php?act=media_frequency',
+		params,
+		true
+	)
 }
 
+export function search(params) {
 
+	return get(
+		'vue_app_content_api.php?act=search',
+
+		params
+	)
+
+
+}
 
 export function model_data_list(params) {
-	
+
 	return get(
-			 'vue_app_content_api.php?act=model_data_list',
-			
-			params
-	) 
-	// console.log('切换全局')
-	// return request({
-	//   url: 'vue_app_content_api.php?act=search',
-	//   method: 'get',
-	//   params
-	// })
-	
-	// if(params.keywords!=''){
-	// }else{
-	// }
-	 
+		'vue_app_content_api.php?act=model_data_list',
+
+		params
+	)
 }
 
 export function get_catlist_data(params) {
-  return get( 
-     'vue_app_content_api.php?act=get_catlist_data',
-    params
-  )
+	return get(
+		'vue_app_content_api.php?act=get_catlist_data',
+		params
+	)
 }
